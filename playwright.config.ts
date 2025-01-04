6da399379c80725999452165cb9159c43e98be29
Dlyons24 <96287175+Dlyons24@users.noreply.github.com>
2025-01-03 22:05:35 -0500

    removed some items

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,11 +39,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
+    
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
